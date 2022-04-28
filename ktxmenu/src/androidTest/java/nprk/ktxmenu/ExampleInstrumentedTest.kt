bb005b52bd64b3b90342bb1d187c36247f172222
nprk <87573445+nprk@users.noreply.github.com>
2022-04-28 17:51:04 +0700

    fix library build.gradle

--- ktxmenu/src/androidTest/java/nprk/ktxmenu/ExampleInstrumentedTest.kt
+++ ktxmenu/src/androidTest/java/nprk/ktxmenu/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("nprk.rateplz.test", appContext.packageName)
+        assertEquals("nprk.ktxmenu.test", appContext.packageName)
     }
 }
